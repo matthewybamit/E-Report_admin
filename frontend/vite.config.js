d45d7b26c1af212c71d47d81790b9298ff1a8622
Renz Matthew Ybamit <renzmatthewy@gmail.com>
2026-02-23 19:57:31 +0800

    Vercel Fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,5 @@
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -6,5 +8,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+
   ],
+  base:process.env.VITE_BASE_URL || '/',
 })
